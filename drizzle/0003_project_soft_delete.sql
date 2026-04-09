c91111abd8e8909023f24f2b081bb403b0bb76b1
drilonhametaj25 <drilonhametaj25@gmail.com>
2026-04-09 14:29:47 +0200

    fix

--- drizzle/0003_project_soft_delete.sql
+++ drizzle/0003_project_soft_delete.sql
@@ -1,3 +1,3 @@
 -- Migrazione: aggiunge deletedAt per soft delete dei progetti
 
-ALTER TABLE "projects" ADD COLUMN "deleted_at" timestamp with time zone;
+ALTER TABLE "projects" ADD COLUMN IF NOT EXISTS "deleted_at" timestamp with time zone;
